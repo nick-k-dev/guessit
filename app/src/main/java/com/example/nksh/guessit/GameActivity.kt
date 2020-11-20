ae89f65966a06569d6f77fd2bc4933e38bc86a28
Steven Harrison <harrs1392@gmail.com>
2020-11-20 13:34:27 -0500

    start game logic

--- app/src/main/java/com/example/nksh/guessit/GameActivity.kt
+++ app/src/main/java/com/example/nksh/guessit/GameActivity.kt
@@ -1,20 +1,62 @@
 package com.example.nksh.guessit
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import com.google.firebase.FirebaseApp
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FileDownloadTask
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
 
 class GameActivity : AppCompatActivity() {
+    private lateinit var storage: FirebaseStorage
+    private var categories = arrayListOf<String>("flower")
+    private var imageRefs  = arrayListOf<StorageReference>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        FirebaseApp.initializeApp(this);
+
+
     }
 
     override fun onStart() {
         super.onStart()
         var toolbarText = findViewById<TextView>(R.id.toolbar_text)
         toolbarText.text = getString(R.string.game_title)
+
+        storage = Firebase.storage
+        for(i in 1..5){
+            var imageString = "gs://quessit-cdffb.appspot.com/images/" + categories[(categories.indices).random()] + "/" + i + ".png"
+            val imageRef = storage.getReferenceFromUrl(imageString)
+            val localFile = File.createTempFile("images$i", ".png")
+
+            imageRef.getFile(localFile).addOnSuccessListener { 
+            }
+            }.addOnFailureListener {
+                // Handle any errors
+            }
+            imageRefs.add(imageRef)
+        }
+
+
+
+
+
+        val img = findViewById<View>(R.id.gameImageView) as ImageView
+        img.setBackgroundResource(R.drawable.game_animation)
+        val frameAnimation = img.background as AnimationDrawable
+
+
     }
 
     fun onGuessButtonClick(view: View) {
