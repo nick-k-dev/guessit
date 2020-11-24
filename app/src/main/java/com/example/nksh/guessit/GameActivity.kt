676ccbafb49df0894c8bd8e3e2375e4e27d27980
Steven Harrison <harrs1392@gmail.com>
2020-11-24 15:49:34 -0500

    working on adding static animation files

--- app/src/main/java/com/example/nksh/guessit/GameActivity.kt
+++ app/src/main/java/com/example/nksh/guessit/GameActivity.kt
@@ -7,18 +7,14 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import com.google.firebase.FirebaseApp
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FileDownloadTask
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.ktx.storage
-import java.io.File
-import java.io.FileOutputStream
-import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.random.Random.Default.nextInt
 
 class GameActivity : AppCompatActivity() {
     private lateinit var storage: FirebaseStorage
-    private var categories = arrayListOf<String>("flower")
+    private var categories = arrayListOf<Int>(R.drawable.flower_animation)
     private var imageRefs  = arrayListOf<StorageReference>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,26 +30,9 @@ class GameActivity : AppCompatActivity() {
         var toolbarText = findViewById<TextView>(R.id.toolbar_text)
         toolbarText.text = getString(R.string.game_title)
 
-        storage = Firebase.storage
-        for(i in 1..5){
-            var imageString = "gs://quessit-cdffb.appspot.com/images/" + categories[(categories.indices).random()] + "/" + i + ".png"
-            val imageRef = storage.getReferenceFromUrl(imageString)
-            val localFile = File.createTempFile("images$i", ".png")
-
-            imageRef.getFile(localFile).addOnSuccessListener { 
-            }
-            }.addOnFailureListener {
-                // Handle any errors
-            }
-            imageRefs.add(imageRef)
-        }
-
-
-
-
 
         val img = findViewById<View>(R.id.gameImageView) as ImageView
-        img.setBackgroundResource(R.drawable.game_animation)
+        img.setBackgroundResource(categories[Random.nextInt(0,categories.size-1)])
         val frameAnimation = img.background as AnimationDrawable
 
 
